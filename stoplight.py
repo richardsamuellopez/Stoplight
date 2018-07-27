d9cf050fe8629d475efa826a459d3c25693bbd2f
richardsamuellopez <richardsamuellopez@gmail.com>
2018-07-26 20:31:37 -0400

    Add sleep hour and yellow sunrise

--- stoplight.py
+++ stoplight.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 hourToWake = 7
-
+hourToSleep = 19
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.OUT)
@@ -13,11 +13,18 @@ GPIO.setup(11, GPIO.OUT)
 try:
   while(True):
     currentHour = datetime.datetime.now().hour
-    if (currentHour >= hourToWake and currentHour < hourToWake+5):
+    currentMin = datetime.datetime.now().minute
+    if (hourToWake-1 == currentHour and currentMin > 44):
       GPIO.output(9, 0)
+      GPIO.output(10, 1)
+      GPIO.output(11, 0)
+    elif (hourToWake <= currentHour < hourToSleep):
+      GPIO.output(9, 0)
+      GPIO.output(10, 0)
       GPIO.output(11, 1)
     else:
       GPIO.output(9, 1)
+      GPIO.output(10, 0)
       GPIO.output(11, 0)
 
     time.sleep(60)
