13c1b89b75772c5970cc414418dc8d711f09569c
richardsamuellopez <richardsamuellopez@gmail.com>
2018-11-06 15:18:47 -0500

    Made stoplight.py functions

--- stoplight.py
+++ stoplight.py
@@ -7,39 +7,48 @@ sleepHour = 19
 wakeMin = 45
 snoozeMin = 30
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+def setup():
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setwarnings(False)
 
-GPIO.setup(9, GPIO.OUT) #Red
-GPIO.setup(10, GPIO.OUT)#Yellow
-GPIO.setup(11, GPIO.OUT)#Green
+  GPIO.setup(9, GPIO.OUT) #Red
+  GPIO.setup(10, GPIO.OUT)#Yellow
+  GPIO.setup(11, GPIO.OUT)#Green
+  return
 
+def party():
+  for x in range(10):
+    for y in range(9, 12):
+      GPIO.output(y, 1)
+      time.sleep(.3)
+    for z in range(9,12):
+      GPIO.output(z, 0)
+      time.sleep(.3)
+  return
 
-for x in range(10):
-  for y in range(9, 12):
-    GPIO.output(y, 1)
-    time.sleep(.3)
-  for z in range(9,12):
-    GPIO.output(z, 0)
-    time.sleep(.3)
+def setlight():
+  currentHour = datetime.datetime.now().hour
+  currentMin = datetime.datetime.now().minute
+  #Yellow
+  if (wakeHour-1 == currentHour and snoozeMin <= currentMin < wakeMin):
+    GPIO.output(9, 0)
+    GPIO.output(10, 1)
+    GPIO.output(11, 0)
+  #Green
+  elif (wakeHour <= currentHour < sleepHour or (wakeHour-1 == currentHour and wakeMin <= currentMin)):
+    GPIO.output(9, 0)
+    GPIO.output(10, 0)
+    GPIO.output(11, 1)
+  #Red
+  else:
+    GPIO.output(9, 1)
+    GPIO.output(10, 0)
+    GPIO.output(11, 0)
 
-currentHour = datetime.datetime.now().hour
-currentMin = datetime.datetime.now().minute
-#Yellow
-if (wakeHour-1 == currentHour and snoozeMin <= currentMin < wakeMin):
-  GPIO.output(9, 0)
-  GPIO.output(10, 1)
-  GPIO.output(11, 0)
-#Green
-elif (wakeHour <= currentHour < sleepHour or (wakeHour-1 == currentHour and wakeMin <= currentMin)):
-  GPIO.output(9, 0)
-  GPIO.output(10, 0)
-  GPIO.output(11, 1)
-#Red
-else:
-  GPIO.output(9, 1)
-  GPIO.output(10, 0)
-  GPIO.output(11, 0)
+  #Do not do cleanup so that current light stay on
+  #GPIO.cleanup()
+  return
 
-#Do not do cleanup so that current light stay on
-#GPIO.cleanup()
+setup()
+party()
+setlight()
